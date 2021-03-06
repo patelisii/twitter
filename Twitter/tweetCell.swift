89e26685988785ffdc9f75d452701cc9505e0ddf
patelisii <34968106+patelisii@users.noreply.github.com>
2021-03-05 20:18:46 -0500

    finished infinite scroll

---
+++ Twitter/tweetCell.swift
@@ -0,0 +1,30 @@
+//
+//  tweetCell.swift
+//  Twitter
+//
+//  Created by Patrick Elisii on 3/5/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class tweetCell: UITableViewCell {
+
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var tweetContentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
